1e945f2e139a1a03366b6a9b8224c8a1bf918c97
ycxu <xu.yanc@husky.neu.edu>
2021-08-08 19:41:38 -0700

    updated proxy

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-      createProxyMiddleware(["/graphql"], { target: "https://moments-backend-yanchen.herokuapp.com/graphql" })
+      createProxyMiddleware(["/graphql"], { target: "https://moments-backend-yanchen.herokuapp.com" })
     );
 };
 
